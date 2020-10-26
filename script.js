41064d873d43103be3005a8b9cd06eb1565ee22c
starrynights7000 <71786295+starrynights7000@users.noreply.github.com>
2020-10-26 13:59:10 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,38 @@
-resize.addEventListener('click', () => sized());
+@@ -0,0 +1,37 @@
+let i = 0;
+while (i < 256) {
+  const div = document.createElement('div');
+  const parent = document.querySelector('.container');
+  div.classList.add('grid-item');
+  div.addEventListener('mouseover', () => div.classList.add('hovered'));
+  parent.appendChild(div);
+  i++;
+}
 
-function createGrid(num) {
-    container.style.setProperty('grid-template-columns', 'repeat(' + num + ', 1fr)');
-    container.style.setProperty('grid-template-columns', `repeat(${num}, 1fr)`);
-    for (let i = 0; i < num * num; i++) {
-        let grid = document.createElement('div');
-        grid.className = 'grid';
-         grid.addEventListener('mouseover', () => grid.classList.add('hovered'));
-        resetB.addEventListener('click', () => grid.classList.remove('hovered'));
-        container.appendChild(grid);
-    }
+function resetGrid() {
+  const parent = document.querySelector('.container');
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+
+  let gridSize = +prompt('Please enter a number between 1 and 50.');
+  while (gridSize < 1 || gridSize > 50) {
+    gridSize = +prompt('Number out of range. Please choose a number between 1 and 50.')
+  }
+
+  const container = document.querySelector('.container');
+  container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+
+  let i = 0
+  while (i < gridSize ** 2) {
+    const div = document.createElement('div');
+    const parent = document.querySelector('.container');
+    div.classList.add('grid-item');
+    div.addEventListener('mouseover', () => div.classList.add('hovered'));
+    parent.appendChild(div);
+    i++;
+  }
 }
-function clear() {
-    while (container.hasChildNodes()) {
-        container.removeChild(container.lastChild);
-    }
-}
-function sized() {
-    clear();
-    let size = prompt('Provide a grid size between 1-50')
-    if (size > 1 && size < 51 && Number.isInteger(+size)) {
-        createGrid(size);
-    }
-    else {
-        alert('Number out of range. Please choose a whole number between 1 and 50');
-             sized();
-    }
-}
-createGrid(16)
+
+const resetBtn = document.querySelector('button')
+resetBtn.addEventListener('click', resetGrid); 
